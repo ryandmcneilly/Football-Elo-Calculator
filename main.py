1b5cbb96ab806a9f48173e03bda8f0f5b445a750
RyanMcNeilly85 <ryanmcn@live.com.au>
2022-12-16 00:28:28 +1000

    Refix previous bug
    It seemed the original implementation was correct, where the away expected chance of winning is the complement

--- main.py
+++ main.py
@@ -127,7 +127,7 @@ def update_points(name_home: str, name_away: str, gd: int) -> None:
     team_home.update_elo(team_home.get_elo() + point_calculation(goal_index(gd),
                          home_result, expected_result(team_home.get_elo(), team_away.get_elo())))
     team_away.update_elo(team_away.get_elo() + point_calculation(goal_index(gd),
-                         away_result, expected_result(team_away.get_elo(), team_home.get_elo())))
+                         away_result, 1 - expected_result(team_away.get_elo(), team_home.get_elo())))
 
 
 def point_calculation(goal_index: float, result: float, expected: float) -> None:
@@ -156,3 +156,4 @@ def read_data() -> None:
 if __name__ == "__main__":
     read_data()
     print_elos()
+
