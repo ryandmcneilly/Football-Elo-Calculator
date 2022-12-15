f395f95674a479a8f3c6f4961a1d06dc23d383cd
RyanMcNeilly85 <ryanmcn@live.com.au>
2022-12-15 21:11:23 +1000

    Housekeeping

--- uqsport_scraper.py
+++ uqsport_scraper.py
@@ -20,34 +20,37 @@ Results={
 """
 
 RESULTS = {}
-DATES = ["2022-12-04","2022-12-11"]
+DATES = ["2022-12-04", "2022-12-11"]
 
 match_id = 0
 
+# Iterates through all possible dates
 for date in DATES:
     url = f"https://teams.uqsport.com.au/Team/Round/1464/null/{date}"
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
     matches = soup.find_all("div", class_="fxt")
-    #iterate through all the matches except the last which is always a bye
+
+    # Iterates through all the matches except the last which is always a bye
     for match in matches[0:-1]:
-        # gets the div team div element and then it's childs text which is the team name
+        # Gets the div team div element and then it's childs text which is the team name
         team_1 = match.find("div", class_="fxt__item--team-home")
         team_2 = match.find("div", class_="fxt__item--team-away")
         team_1_name = team_1.find("a").text
         team_2_name = team_2.find("a").text
-        #team goals
-        team_1_goals = int(match.find("div", class_="fxt__item--score-home").contents[0])
-        team_2_goals = int(match.find("div", class_="fxt__item--score-away").contents[0])
-        #calculates who won the match
-        if team_1_goals > team_2_goals:
-            winner = 1
-        elif team_1_goals < team_2_goals:
-            winner = 2
-        else:
-            winner = 0
-        #stores the data in the dictionary and increment match_id
-        RESULTS[f"match_{match_id}"]={
+
+        # Team goals
+        team_1_goals = int(match.find(
+            "div", class_="fxt__item--score-home").contents[0])
+        team_2_goals = int(match.find(
+            "div", class_="fxt__item--score-away").contents[0])
+
+        # Calculates who won the match
+        winner = 0 if team_1_goals == team_2_goals else int(
+            team_1_goals < team_2_goals) + 1
+
+        # Stores the data in the dictionary and increment match_id
+        RESULTS[f"match_{match_id}"] = {
             "team_1": {
                 "name": team_1_name,
                 "goals": team_1_goals,
@@ -61,7 +64,7 @@ for date in DATES:
         }
         match_id += 1
 
-#converts the dictionary into json and saves it
+# Converts the dictionary into json and saves it
 RESULTS = json.dumps(RESULTS, indent=4)
 with open('game_data.json', 'w') as outfile:
     outfile.write(RESULTS)
